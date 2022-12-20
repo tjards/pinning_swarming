6e7ed588e73a62d55b7e6851347ee8494e31cb81
tjards <travis.jardine@gmail.com>
2022-12-20 18:25:02 -0500

    basic graph theory terms implemented

--- pinning_tools.py
+++ pinning_tools.py
@@ -17,17 +17,14 @@ Preliminaries:
     - G is undirected: (a,b) \in E <=> (b,a) \in E
     - Nodes i,j are neighbours if they share an edge, (i,j) /in E
     - d1=|N_1| is the degree of Node 1, or, the number of neighbours
-    - Adjacency Matrix, A = {a_ij} s.t. 1 if i,j are neighbours, 0 if not
-    - Degree Matrix, D = diag{d1,d2,...dN}
-    - A Component set of a graph is a set with no neighbours outside itself
 
 Created on Tue Dec 20 13:32:11 2022
-
 
 References:
     http://kth.diva-portal.org/smash/get/diva2:681041/FULLTEXT01.pdf
 
 @author: tjards
+    
 """
 
 import numpy as np
@@ -43,6 +40,7 @@ data = np.load('Data/state_25.npy') # 25 nodes, 6 states [x,y,z,vx,vy,vz]
 
 #%% Compute the Adjacency Matrix
 # ------------------------------
+# A = {a_ij} s.t. 1 if i,j are neighbours, 0 if not
 def compute_adj_matrix(data,r):
     # initialize
     nNodes  = data.shape[1]             # number of agents (nodes)
@@ -63,21 +61,17 @@ def compute_adj_matrix(data,r):
     assert (A == A.transpose()).all()
     # return the matrix
     return A
-
-# try it
-r = 2 # range to be considered a neighbour 
-A = compute_adj_matrix(data, r)                
+               
             
 #%% Compute the Degree Matrix
 # ------------------------------
+# D = diag{d1,d2,...dN}
 def compute_deg_matrix(data,r):
     # initialize
     nNodes  = data.shape[1]             # number of agents (nodes)
     D       = np.zeros((nNodes,nNodes)) # initialize degree matrix as zeros
     # for each node
     for i in range(0,nNodes):
-        # initialize the degree
-        degree = 0
         # search through neighbours
         for j in range(0,nNodes):
             # skip self
@@ -102,19 +96,62 @@ def compute_lap_matrix(A,D):
     assert L.sum() == 0
     # ensure Positive Semi-Definite (all eigen values are >= 0)
     assert (eigs >= 0).all()
-    # how many components
-    nComp = np.count_nonzero(eigs==0)
-    print('the graph has ', nComp, ' component(s)')
     # return the matrix
-    return L, nComp
+    return L
     
+#%% Compute Components: Component set of a graph is a set with no 
+#   neighbours outside itself. The number of null eigen values gives 
+#   the number of components.
+# ------------------------------------------------------------------
+#   
+def compute_comp(L):
+    eigs = np.linalg.eigvals(L)         # eigen values 
+    # how many components (how many zero eig values)
+    nComp = np.count_nonzero(eigs==0)
+    #print('the graph has ', nComp, ' component(s)')
+    return nComp
+
+#%% Compute Augmented Laplacian: The number of null eigen values is 
+#   the number of components in the graph that do not contain pins.
+#   generally, the larger the aug connectivity, the better.
+# ----------------------------------------------------------------- 
+def compute_aug_lap_matrix(L,P,gamma,rho):
+    L_aug = np.multiply(gamma, L) + np.multiply(rho, P)
+    eigs = np.linalg.eigvals(L_aug)         # eigen values
+    # ensure Positive Semi-Definite (all eigen values are >= 0)
+    assert (eigs >= 0).all()
+    # tell me if not fully pinned (i.e. there are null eigen values)
+    if np.count_nonzero(eigs==0) > 0:
+        print('note: graph is not fully pinned')
+    # compute the augmented connectivity (smallest eig value)
+    aug_connectivity = np.amin(eigs)
+    # and the index
+    aug_connectivity_i = np.argmin(eigs)
+    # return the matrix, augmented connectivity, and index
+    return L_aug, aug_connectivity, aug_connectivity_i
 
 
 # %%try it
-# -------
-r = 2 # range to be considered a neighbour 
+# ---------
+r = 2         # range to be considered a neighbour 
+gamma   = 1   # coupling strength
+rho     = 1   # pinning strength
 A = compute_adj_matrix(data, r)  
 D = compute_deg_matrix(data, r)   
-L, nComp = compute_lap_matrix(A,D)   
+L = compute_lap_matrix(A,D)   
+nComp = compute_comp(L) 
+
+
+# let us set pin (manually)
+# -------------------------
+P = np.zeros((data.shape[1],data.shape[1])) # initialize Pin Matrix
+P[0,0] = 1
+P[5,5] = 1
+
+# compute augmented connectivity 
+L_aug, aug_connectivity, aug_connectivity_i = compute_aug_lap_matrix(L,P,gamma,rho)
+
+
+
     
     
