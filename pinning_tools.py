36f32329bbf8b41ab93ec9e2ff8d3d077084afec
tjards <travis.jardine@gmail.com>
2022-12-20 17:06:33 -0500

    started building pinning module

--- pinning_tools.py
+++ pinning_tools.py
@@ -19,6 +19,7 @@ Preliminaries:
     - d1=|N_1| is the degree of Node 1, or, the number of neighbours
     - Adjacency Matrix, A = {a_ij} s.t. 1 if i,j are neighbours, 0 if not
     - Degree Matrix, D = diag{d1,d2,...dN}
+    - A Component set of a graph is a set with no neighbours outside itself
 
 Created on Tue Dec 20 13:32:11 2022
 
@@ -36,37 +37,84 @@ data = np.load('Data/state_25.npy') # 25 nodes, 6 states [x,y,z,vx,vy,vz]
 
 #%% Kronrcker product (demo)
 # ------------------------
-A1 = np.eye(2)          #   A = m x n matrix
-B1 = data               #   B = p x q matrix
-Kron = np.kron(A1,B1)   #   Kron = pm x qn matrix
+#A1 = np.eye(2)          #   A = m x n matrix
+#B1 = data               #   B = p x q matrix
+#Kron = np.kron(A1,B1)   #   Kron = pm x qn matrix
 
 #%% Compute the Adjacency Matrix
-# ----------------------------
-# Let us say nodes are adjacent, if they are <r distance away
+# ------------------------------
+def compute_adj_matrix(data,r):
+    # initialize
+    nNodes  = data.shape[1]             # number of agents (nodes)
+    A       = np.zeros((nNodes,nNodes)) # initialize adjacency matrix as zeros
+    # for each node
+    for i in range(0,nNodes):  
+        # search through neighbours
+        for j in range(0,nNodes):
+            # skip self
+            if i != j:
+                # compute distance
+                dist = np.linalg.norm(data[0:3,j]-data[0:3,i])
+                # if close enough
+                if dist < r:
+                    # mark as neighbour
+                    A[i,j] = 1
+    # ensure A = A^T
+    assert (A == A.transpose()).all()
+    # return the matrix
+    return A
 
-nNodes  = data.shape[1]             # number of agents (nodes)
-A       = np.zeros((nNodes,nNodes)) # initialize adjacency matrix as zeros
-r       = 2                         # range to be considered a neighbour  
-
-# for each node
-for i in range(0,nNodes):  
-    # search through neighbours
-    for j in range(0,nNodes):
-        # skip self
-        if i != j:
-            # compute distance
-            dist = np.linalg.norm(data[0:3,j]-data[0:3,i])
-            # if close enough
-            if dist < r:
-                # mark as neighbour
-                A[i,j] = 1
-
-# ensure A = A^T
-assert (A == A.transpose()).all()
-
-                
+# try it
+r = 2 # range to be considered a neighbour 
+A = compute_adj_matrix(data, r)                
             
+#%% Compute the Degree Matrix
+# ------------------------------
+def compute_deg_matrix(data,r):
+    # initialize
+    nNodes  = data.shape[1]             # number of agents (nodes)
+    D       = np.zeros((nNodes,nNodes)) # initialize degree matrix as zeros
+    # for each node
+    for i in range(0,nNodes):
+        # initialize the degree
+        degree = 0
+        # search through neighbours
+        for j in range(0,nNodes):
+            # skip self
+            if i != j:
+                # compute distance
+                dist = np.linalg.norm(data[0:3,j]-data[0:3,i])
+                # if close enough
+                if dist < r:
+                    # mark as neighbour
+                    D[i,i] += 1
+    # return the matrix
+    return D
+
+#%% Compute the graph Laplacian
+# -----------------------------
+def compute_lap_matrix(A,D):
+    L = D-A
+    eigs = np.linalg.eigvals(L)         # eigen values 
+    # ensure L = L^T
+    assert (L == L.transpose()).all()
+    # ensure has zero row sum
+    assert L.sum() == 0
+    # ensure Positive Semi-Definite (all eigen values are >= 0)
+    assert (eigs >= 0).all()
+    # how many components
+    nComp = np.count_nonzero(eigs==0)
+    print('the graph has ', nComp, ' component(s)')
+    # return the matrix
+    return L, nComp
     
-    
+
+
+# %%try it
+# -------
+r = 2 # range to be considered a neighbour 
+A = compute_adj_matrix(data, r)  
+D = compute_deg_matrix(data, r)   
+L, nComp = compute_lap_matrix(A,D)   
     
     
