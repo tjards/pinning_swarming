47b0958d5e651e19922cfb0b2fcf7b1fe355f42a
tjards <travis.jardine@gmail.com>
2022-12-25 22:49:10 -0500

    better component finder

--- main.py
+++ main.py
@@ -54,9 +54,9 @@ from utils import pinning_tools, lemni_tools, starling_tools, swarm_metrics, too
 # ------------------
 #np.random.seed(1)
 Ti      =   0         # initial time
-Tf      =   50        # final time 
+Tf      =   60        # final time 
 Ts      =   0.02      # sample time
-nVeh    =   25         # number of vehicles
+nVeh    =   50         # number of vehicles
 iSpread =   50         # initial spread of vehicles
 tSpeed  =   0.001         # speed of target
 rVeh    =   1         # physical radius of vehicle 
