3f8fc6a356c910e3edb425ab5858f670d589da0c
tjards <travis.jardine@gmail.com>
2023-01-04 18:57:25 -0500

    developing some plots

--- main.py
+++ main.py
@@ -374,6 +374,7 @@ ax.fill_between(t_all[4::], metrics_order_all[4::,7], color = 'green', alpha = 0
 #note: can include region to note shade using "where = Y2 < Y1
 ax.set(xlabel='Time [s]', title='Energy Consumption')
 ax.set_ylabel('Total Acceleration [m^2]', color = 'g')
+ax.tick_params(axis='y',colors ='green')
 ax.set_xlim([0, Tf])
 ax.set_ylim([0, 10])
 #ax.plot([70, 70], [100, 250], '--b', lw=1)
@@ -392,7 +393,7 @@ ax2.plot(t_all[4::],1-metrics_order_all[4::,0], color='tab:blue', linestyle = '-
 ax2.set(title='Energy Consumption')
 ax2.set_ylabel('Disorder of the Swarm', color='tab:blue')
 #ax2.invert_yaxis()
-
+ax2.tick_params(axis='y',colors ='tab:blue')
 ax2.text(Tf-Tf*0.3, 0.1, 'Total Energy: ' + str(round(total_e,1)), style='italic',
         bbox={'facecolor': 'green', 'alpha': 0.1, 'pad': 1})
 
@@ -413,6 +414,7 @@ ax.fill_between(t_all[4::], metrics_order_all[4::,9], metrics_order_all[4::,11],
 #note: can include region to note shade using "where = Y2 < Y1
 ax.set(xlabel='Time [s]', title='Spacing between Agents [m]')
 ax.set_ylabel('Mean Distance [m]', color = 'g')
+ax.tick_params(axis='y',colors ='green')
 ax.set_xlim([0, Tf])
 ax.set_ylim([0, 20])
 total_e = np.sqrt(np.sum(cmds_all**2))
@@ -421,10 +423,12 @@ total_e = np.sqrt(np.sum(cmds_all**2))
 ax2 = ax.twinx()
 ax2.set_xlim([0, Tf])
 ax2.set_ylim([0, 2*nVeh])
-ax2.plot(t_all[4::],metrics_order_all[4::,10], color='tab:blue', linestyle = '--')
+ax2.plot(t_all[4::],metrics_order_all[4::,10], color='tab:blue', linestyle = '-')
 ax2.set_ylabel('Number of Connections', color='tab:blue')
+ax2.tick_params(axis='y',colors ='tab:blue')
 #ax2.invert_yaxis()
 
+ax.legend(['Within Range', 'Oustide Range'], loc = 'upper left')
 ax.grid()
 #fig.savefig("test.png")
 plt.show()
